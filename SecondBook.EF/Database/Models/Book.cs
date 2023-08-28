da4343bb4433d3039f96346af5248b04d354b79f
josip platuzic <josip.platuzic@spandevacademy.onmicrosoft.com>
2023-08-28 18:20:54 +0200

    added Author entity

--- SecondBook.EF/Database/Models/Book.cs
+++ SecondBook.EF/Database/Models/Book.cs
@@ -8,6 +8,8 @@
         public string Description { get; set; }
         public DateTime PublishedDate { get; set; }
         public int? CategoryId { get; set; }
+        public int? AuthorId { get; set; }
         public Category? Category { get; set; }
+        public Author? Author { get; set; }
     }
 }
--- SecondBook.EF/Database/SecondBookDBContext.cs
+++ SecondBook.EF/Database/SecondBookDBContext.cs
@@ -8,6 +8,7 @@ namespace SecondBook.EF.Database
         public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -20,6 +21,12 @@ namespace SecondBook.EF.Database
                 .HasOne(e => e.Category)
                 .WithMany(e => e.Books)
                 .HasForeignKey(e => e.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Book>()
+                .HasOne(e => e.Author)
+                .WithMany(e => e.Books)
+                .HasForeignKey(e => e.AuthorId)
                 .OnDelete(DeleteBehavior.SetNull);
         }
     }
